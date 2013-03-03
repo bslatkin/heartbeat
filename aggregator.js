c4605471aa9466422c299667b89e48000f15012d
Brett Slatkin <brett@haxor.com>
2013-03-02 17:43:54 -0800

    Bucket for aggregating data points and tests

--- aggregator.js
+++ aggregator.js
@@ -1,74 +1,96 @@
 /* Copyright 2013 Brett Slatkin
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
 
-var BucketView = function() {
-
-};
-
-BucketView.prototype.filter = function(keyValues) {
-
-};
-
-BucketView.prototype.groupBy = function(key) {
-
-};
-
-BucketView.prototype.count = function() {
-
-};
-
-
-var Bucket = function(periodSeconds, tickCallback) {
+var Bucket = function(windowMs) {
+    this.windowMs = windowMs;
     this.points = [];
 };
 
-Bucket.prototype.addPoint = function(point) {
+Bucket.prototype.add = function(point) {
+    if (!point['kv']) {
+        point['kv'] = {};
+    }
+    point.kv['_data'] = point;
+    point.kv['_created'] = (new Date()).getTime();
+    this.points.push(point.kv);
+    delete point.kv;  // Remove circular reference
+    console.log('Add - New state: ' + JSON.stringify(this.points));
+};
 
+Bucket.prototype.expire_ = function(now) {
+    var now = (new Date()).getTime();
+    var newPoints = [];
+    for (var i = 0, n = this.points.length; i < n; i++) {
+        var point = this.points[i];
+        var nextTime = point._created;
+        if (now - nextTime < this.windowMs) {
+            newPoints.push(point);
+        }
+    }
+    this.points = newPoints;
+    console.log('Expire - New state: ' + JSON.stringify(this.points));
 };
 
 Bucket.prototype.query = function() {
-    // Returns a BucketView
+    this.expire_();
+    var result = [];
+    for (var i = 0, n = this.points.length; i < n; i++) {
+        var point = this.points[i];
+        result.push(point);
+    }
+    return result;
 };
-
-Bucket.prototype.start = function() {
-    // Begin polling for data expiration, etc
-};
-
-
-
-// Something like:
-//
-// var updateCount = bucket.filter({update: 1}).group_by('c').count();
-// var leaveCount = bucket.filter({join: 1}).group_by('c').count();
-
-// var usersOnSite = updateCount - leaveCount;
-
-
-// TEST_DATA = [
-//   {"c":"rando464780","leave":1,"_data":{"n": 1}}
-//   {"c":"rando464780","update":1,"_data":{"n": 1}}
-// ];
-
-
-{"c":"rando464780","leave":1,"__point":{"n": 1}}
 
 
 function init() {
+    var data = [
+        {'kv':{'c':'rando464780','update':1},'n': 1},
+        {'kv':{'c':'rando464781','update':1},'n': 1},
+        {'kv':{'c':'rando464782','update':1},'n': 1},
+        {'kv':{'c':'rando464783','update':1},'n': 1},
+        {'kv':{'c':'rando464784','update':1},'n': 1},
+        {'kv':{'c':'rando464785','update':1},'n': 1},
+        {'kv':{'c':'rando464786','update':1},'n': 1},
+    ];
+    var bucket = new Bucket(1000);
+    $.each(data, function(i, x) {
+        bucket.add(JSON.parse(JSON.stringify(x)));
+    });
+    setTimeout(function() {
+        var result = bucket.query();
+        console.log('After 500ms! ' + result.length);
+        console.log(JSON.stringify(result));
+        $.each(data, function(i, x) {
+            bucket.add(JSON.parse(JSON.stringify(x)));
+        });
+        var result = bucket.query();
+        console.log('After 500ms! second ' + result.length);
 
-    // $('#data-points')
+        setTimeout(function() {
+            var result = bucket.query();
+            console.log('After 1200ms! ' + result.length);
+            console.log(JSON.stringify(result));
+
+            setTimeout(function() {
+                var result = bucket.query();
+                console.log('After 1600ms! ' + result.length);
+                console.log(JSON.stringify(result));
+            }, 400);
+        }, 700);
+    }, 500);
 }
 
 
