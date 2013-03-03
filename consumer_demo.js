64cd62aacf86f8bd415ac876c354073a0738f137
Randall Leeds <randall.leeds@gmail.com>
2013-03-02 17:21:08 -0800

    leaking vars, new Bucket constructor

--- consumer_demo.js
+++ consumer_demo.js
@@ -1,11 +1,10 @@
 function handleSubmit(e) {
   e.preventDefault()
 
-  var bucket = new Bucket(5000, function () {
-    // Handle tick
-  })
 
-  channelId = $(e.target).find('input[name=channelId]')[0].value
+  var bucket = new Bucket(5000)
+  , channelId = $(e.target).find('input[name=channelId]')[0].value
+
   pollChannel(channelId, function (message) {
     for (var annotation in message.annotations) {
       if (annotation.type == 'net.app.contrib.timeseries') {
