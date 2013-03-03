43e718091da3d95ec3cc9791c6238c30a12dd7a3
Brett Slatkin <brett@haxor.com>
2013-03-02 18:29:27 -0800

    Making the default page the graph

--- consumer_demo.js
+++ consumer_demo.js
@@ -1,3 +1,18 @@
+/* Copyright 2013 Randall Leeds
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var BUCKET_WINDOW = 10000
 var UPDATE_INTERVAL = 1000
 
@@ -34,11 +49,8 @@ function updateGraph(data) {
 }
 
 
-function handleSubmit(e) {
-  e.preventDefault()
-
+function startListening(channelId) {
   var bucket = new Bucket(BUCKET_WINDOW)
-  , channelId = $(e.target).find('input[name=channelId]')[0].value
   , updated = new Date()
   , dataPoints = []
 
@@ -82,7 +94,7 @@ function handleSubmit(e) {
 
 function init() {
   if (!authorized()) { return }
-  $('#send-form').submit(handleSubmit)
+  startListening(6291);
 }
 
 
